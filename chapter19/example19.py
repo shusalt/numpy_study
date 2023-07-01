a6923822295de53db274ee98e0aaea7001f12448
shusalt <1356467790@qq.com>
2023-07-01 16:28:06 +0800

    增加了第19章

--- chapter19/example19.py
+++ chapter19/example19.py
@@ -1,6 +1,7 @@
 import numpy as np
 '''numpy 字节交换'''
 if __name__ == '__main__':
+    '''numpy.byteswap() --> 元素的字节进行大小端交换'''
     a=np.array([1,256,8755],dtype=np.int16)
     print(a)
     print(map(hex,a))
